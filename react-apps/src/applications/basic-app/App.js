670dc9a3374c55be97daac47bd06f2e475f4aa09
ZviArono <zvi.aronwoitz@gmail.com>
2022-11-30 20:14:23 +0200

    use props in component

--- react-apps/src/applications/basic-app/App.js
+++ react-apps/src/applications/basic-app/App.js
@@ -1,5 +1,17 @@
+function SayHello(props) {
+  console.log(props);
+  return <div style={{ color: props.color }}>Hello {props.name}</div>;
+}
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <div> Hello </div>
+      <p>to React</p>
+      <SayHello name="yoram" color="red" />
+      <SayHello name="Zvika" color="blue" />
+    </div>
+  );
 }
 
 export default App;
